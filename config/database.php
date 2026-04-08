5b9dceb84002c3de1a3d6aec01fcd4bf1cf74682
Ricardo <ricardo.castellarp@gmail.com>
2026-04-07 23:32:59 -0500

    db config primer intento

--- config/database.php
+++ config/database.php
@@ -1,6 +1,11 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "task_manager");
+$host = "localhost";
+$user = "u744129407_ricapa";
+$password = "861019rCp+";
+$database = "u744129407_tasks";
+
+$conn = new mysqli($host, $user, $password, $database);
 
 if($conn->connect_error){
-    die(json_encode(["error" => "DB connection failed" ]));
+    die(json_encode(["error" => "DB connection failed: " . $conn->connect_error ]));
 }

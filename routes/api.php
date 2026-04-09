0096a1035ff53a6de27e55cefd1b50c278522914
Ricardo <ricardo.castellarp@gmail.com>
2026-04-08 21:25:22 -0500

    fix: correcion de manejador de ruta para prevenir la accion del JWT middleware que bloquea los endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ ."/../middleware/auth_jwt.php";
+
 require_once __DIR__ . "/../controllers/AuthController.php";
 require_once __DIR__ . "/../controllers/UserController.php";
 require_once __DIR__ . "/../controllers/TaskController.php";
@@ -19,30 +19,35 @@ if($route === 'register' && $method === 'POST'){
 
 // Listar usuarios (role:admin)
 if ($route === 'users' && $method === 'GET') {
+    require_once __DIR__ ."/../middleware/auth_jwt.php";
     getUsers($user);
     exit();
 }
 
 //Crear usuario nuevo (role: user/admin)
 if($route === 'users' && $method === 'POST'){
+    require_once __DIR__ ."/../middleware/auth_jwt.php";
     createUser($user);
     exit();
 }
 
 // Editar usuario (role:admin)
 if($route === 'users' && $method === 'PUT'){
+    require_once __DIR__ ."/../middleware/auth_jwt.php";
     updateUser($user);
     exit();
 }
 
 // Eliminar usuario (role:admin)
 if($route === 'users' && $method === 'DELETE'){
+    require_once __DIR__ ."/../middleware/auth_jwt.php";
     deleteUser($user);
     exit();
 }
 
 // Traer tareas
 if($route === 'tasks' && $method === 'GET'){
+    require_once __DIR__ ."/../middleware/auth_jwt.php";
     getTasks($user);
     // echo json_encode(["mensaje" => "Middleware saltado"]);
     exit();
@@ -50,18 +55,21 @@ if($route === 'tasks' && $method === 'GET'){
 
 // Crear tarea
 if($route ==='tasks' && $method === 'POST'){
+    require_once __DIR__ ."/../middleware/auth_jwt.php";
     createTask($user);
     exit();
 }
 
 // Editar tarea
 if($route === 'tasks' && $method === 'PUT'){
+    require_once __DIR__ ."/../middleware/auth_jwt.php";
     updateTask($user);
     exit();
 }
 
 // Eliminar tarea
 if($route === 'tasks' && $method === 'DELETE'){
+    require_once __DIR__ ."/../middleware/auth_jwt.php";
     deleteTask($user);
     exit();
 }

c93bdcd2f16e30875addb77c84f05b3f704c0f28
Ricardo <ricardo.castellarp@gmail.com>
2026-04-09 05:26:14 -0500

    preflight config set

--- index.php
+++ index.php
@@ -9,6 +9,11 @@ header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Content-Type: application/json");
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 // Obtener URI
 $request  = $_SERVER['REQUEST_URI'];
 
